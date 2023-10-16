5086a4122847e87d69b5a1cf7e6e14686258db15
Cameku <cameku@gmail.com>
2023-10-16 12:41:45 +0100

    Pages created
    Api helper class to fetch data using async/ await function
    Home page -  with useEffect to call getData function
    Data type class created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
+    < Home />
+
+     {/*  <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -17,7 +20,7 @@ function App() {
           rel="noopener noreferrer" >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
